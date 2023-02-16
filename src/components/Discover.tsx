a46cec23c6bec25534c6d37fe5b530691efcc378
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-15 16:44:29 -0800

    updated

--- src/components/Discover.tsx
+++ src/components/Discover.tsx
@@ -5,13 +5,41 @@ const Discover = () => {
   return (
     <div>
       <Container>
-        <Wrapper></Wrapper>
+        <Wrapper>
+          <h1>
+            Discover more. <span>Good things are waiting for you</span>
+          </h1>
+          <Card>
+            <One>
+              <DivOne>
+                <p>Explore new arrivals</p>
+                <h2>Shop the latest from top brands</h2>
+              </DivOne>
+              <DivTwo>
+                <Button>Show me all</Button>
+              </DivTwo>
+            </One>
+            <Two></Two>
+          </Card>
+        </Wrapper>
       </Container>
     </div>
   );
 };
 
 export default Discover;
+
+const Two = styled.div``;
+
+const Button = styled.button``;
+
+const DivTwo = styled.div``;
+
+const DivOne = styled.div``;
+
+const One = styled.div``;
+
+const Card = styled.div``;
 
 const Wrapper = styled.div``;
 
