12f34e945de397d1c04fe733ae04d523310cc2ad
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-21 10:57:37 +0100

    saved

--- src/components/ALLroutes/AllRoutes.tsx
+++ src/components/ALLroutes/AllRoutes.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
 import HomeScreen from "../HomeScreen";
+import SinglePage from "../Singlepage/SinglePage";
 
 const AllRoutes = () => {
   let routes = useRoutes([
@@ -8,8 +9,48 @@ const AllRoutes = () => {
       path: "/",
       element: <HomeScreen />,
     },
+    {
+      path: "/details/:id",
+      element: <SinglePage />,
+    },
   ]);
   return <div>{routes}</div>;
 };
 
 export default AllRoutes;
+
+/**import React from "react";
+import { useRoutes } from "react-router-dom";
+import CartPage from "../CartPage/CartPage";
+import Products from "../Products/Products";
+import Register from "../Register/Register";
+import SinglePage from "../SinglePage/SinglePage";
+import HomeScreen from "./HomeScreen";
+
+const AllRoutes = () => {
+	let element = useRoutes([
+		{
+			path: "/",
+			element: <HomeScreen />,
+		},
+		{
+			path: "/details/:id",
+			element: <SinglePage />,
+		},
+		{
+			path: "/cart",
+			element: <CartPage />,
+		},
+		{
+			path: "/product",
+			element: <Products />,
+		},
+		{
+			path: "/register",
+			element: <Register />,
+		},
+	]);
+	return element;
+};
+
+export default AllRoutes; */
