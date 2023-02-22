baf157e8cfb60b2f6297e726575ab0c727f7d451
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-22 07:30:11 +0100

    updating

--- src/components/Global/ReduxState.tsx
+++ src/components/Global/ReduxState.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction } from "@reduxjs/toolkit";
 
-const ReduxState = () => {
-  return <div>ReduxState</div>;
+interface userData {
+  name: string;
+  email: string;
+  password: string;
+  _id: string;
+}
+
+interface cartData {
+  title: string;
+  description: string;
+  price: number;
+  category: string;
+  _id: string;
+  cartQuantity: number;
+}
+
+const initialState = {
+  currentUser: {} as userData | null,
+  cart: [] as Array<cartData>,
 };
 
-export default ReduxState;
+const ReduxState = createSlice({
+  name: "second",
+  initialState,
+  reducers: {},
+});
+
+export const {} = ReduxState.actions;
+
+export default ReduxState.reducer;
