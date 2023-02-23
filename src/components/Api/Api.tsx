43839530feaef13b48b0e6cd93f81244d1466cda
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-23 18:58:16 +0100

    saved

--- src/components/Api/Api.tsx
+++ src/components/Api/Api.tsx
@@ -1,3 +1,21 @@
 import axios from "axios";
 
 const endPoint = "https://localhost:4573/api";
+
+interface userData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const createUser = async ({ name, email, password }: userData) => {
+  await axios
+    .post(`${endPoint}/register`, {
+      name,
+      email,
+      password,
+    })
+    .then((res) => {
+      console.log(res);
+    });
+};
